04c7128568c091dbbf5b19fa7b64fbcc81e3d72a
Felipe Lima <felipe.lima.092@ufrn.edu.br>
2023-12-19 23:32:51 -0300

    feat: configuração do config server funcionando

--- storage-service/src/main/java/com/pixelwave/imagestorage/PixelWaveImageStorageServiceApplication.java
+++ storage-service/src/main/java/com/pixelwave/imagestorage/PixelWaveImageStorageServiceApplication.java
@@ -8,8 +8,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class PixelWaveImageStorageServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PixelWaveImageStorageServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PixelWaveImageStorageServiceApplication.class, args);
+    }
 
 }
