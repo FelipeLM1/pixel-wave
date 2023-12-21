d969de147c333fa210e771a4c0a699abe74dc925
Felipe Lima <felipe.lima.092@ufrn.edu.br>
2023-12-21 00:21:26 -0300

    feat: microsservices fazendo consulta ao ms de storage para buscar a imagem

--- color-service/src/main/java/com/pixelwave/colorservice/service/ColorImageService.java
+++ color-service/src/main/java/com/pixelwave/colorservice/service/ColorImageService.java
@@ -1,29 +1,76 @@
 package com.pixelwave.colorservice.service;
 
+import com.pixelwave.colorservice.dto.ResourceResponseDTO;
 import com.pixelwave.colorservice.model.ColorFilterEnum;
+import com.pixelwave.colorservice.service.request.StorageClient;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 import net.coobird.thumbnailator.Thumbnails;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
+import java.util.Objects;
 
 @Service
 public class ColorImageService {
 
-    public Resource applyFilter(Long imageId, ColorFilterEnum filter) throws IOException {
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        var inputImage = new File("uploads/teste.jpeg");
+    private static final Logger logger = LoggerFactory.getLogger(ColorImageService.class);
+
+    private final StorageClient storageClient;
+
+    public ColorImageService(StorageClient storageClient) {
+        this.storageClient = storageClient;
+    }
+
+    @Retry(name = "storage-service", fallbackMethod = "fallbackRetryApplyFilter")
+    @CircuitBreaker(name = "storage-service", fallbackMethod = "fallbackCircuitBreakerApplyFilter")
+    @RateLimiter(name = "storage-service", fallbackMethod = "fallbackRateLimiterApplyFilter")
+    @Bulkhead(name = "storage-service", fallbackMethod = "fallbackBulkheadApplyFilter", type = Bulkhead.Type.THREADPOOL)
+    public ResourceResponseDTO applyFilter(Long imageId, ColorFilterEnum filter) throws IOException {
+        var imageRes = storageClient.findImageById(imageId).getBody();
+        if (Objects.isNull(imageRes)) {
+            throw new IllegalArgumentException("Erro ao encontrar imagem requisitada.");
+        }
+
+        var outputStream = new ByteArrayOutputStream();
+
         Thumbnails
-                .of(inputImage)
+                .of(imageRes.getInputStream())
                 .scale(1.0)
                 .addFilter(filter.getFilter())
                 .toOutputStream(outputStream);
 
-        return new ByteArrayResource(outputStream.toByteArray());
+        return new ResourceResponseDTO(imageRes.getFilename(), new ByteArrayResource(outputStream.toByteArray()));
     }
 
+    private ResourceResponseDTO fallbackCircuitBreakerApplyFilter(Long imageId, ColorFilterEnum filter, Throwable throwable) {
+        logger.error("CircuitBreaker Fallback - Falha ao processar a requisição: id: {}, filter: {}", imageId, filter);
+        logger.error("Fallback acionado devido a uma exceção: {}", throwable.getMessage());
+        return null;
+    }
+
+    private ResourceResponseDTO fallbackBulkheadApplyFilter(Long imageId, ColorFilterEnum filter, Throwable throwable) {
+        logger.error("Bulkhead Fallback - Falha ao processar a requisição: id: {}, filter: {}", imageId, filter);
+        logger.error("Fallback acionado devido a uma exceção: {}", throwable.getMessage());
+        return null;
+    }
+
+    private ResourceResponseDTO fallbackRetryApplyFilter(Long imageId, ColorFilterEnum filter, Throwable throwable) {
+        logger.error("Retry Fallback - Falha ao processar a requisição: id: {}, filter: {}", imageId, filter);
+        logger.error("Fallback acionado devido a uma exceção: {}", throwable.getMessage());
+        return null;
+    }
+
+    private ResourceResponseDTO fallbackRateLimiterApplyFilter(Long imageId, ColorFilterEnum filter, Throwable throwable) {
+        logger.error("RateLimiter Fallback - Falha ao processar a requisição: id: {}, filter: {}", imageId, filter);
+        logger.error("Fallback acionado devido a uma exceção: {}", throwable.getMessage());
+        return null;
+    }
 
 }
