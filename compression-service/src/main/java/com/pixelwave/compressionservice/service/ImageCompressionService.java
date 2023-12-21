943e1ae2f28206a763dfd57ebd11d50026a028d8
Felipe Lima <felipe.lima.092@ufrn.edu.br>
2023-12-21 00:22:37 -0300

    feat: atualização nos services utilizando métodos de resiliência

--- compression-service/src/main/java/com/pixelwave/compressionservice/service/ImageCompressionService.java
+++ compression-service/src/main/java/com/pixelwave/compressionservice/service/ImageCompressionService.java
@@ -1,38 +1,83 @@
 package com.pixelwave.compressionservice.service;
 
 import com.pixelwave.compressionservice.dto.CompressImageDTO;
+import com.pixelwave.compressionservice.dto.ResourceResponseDTO;
+import com.pixelwave.compressionservice.service.request.StorageClient;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 import net.coobird.thumbnailator.Thumbnails;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
+import java.util.Objects;
 
 @Service
 public class ImageCompressionService {
-    public Resource compressImage(CompressImageDTO compressImageDTO) {
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        //TODO pegar a imagem do servico de storage
-        var inputImage = new File("uploads/teste.jpeg");
 
-        try {
-            Thumbnails
-                    .of(inputImage)
-                    .outputQuality(compressImageDTO.compressionQuality())
-                    .scale(1.0)
-                    .toOutputStream(outputStream);
+    private static final Logger logger = LoggerFactory.getLogger(ImageCompressionService.class);
 
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    private final StorageClient storageClient;
+
+    public ImageCompressionService(StorageClient storageClient) {
+        this.storageClient = storageClient;
+    }
+
+    @Retry(name = "storage-service", fallbackMethod = "fallbackRetryApplyFilter")
+    @CircuitBreaker(name = "storage-service", fallbackMethod = "fallbackCircuitBreakerApplyFilter")
+    @RateLimiter(name = "storage-service", fallbackMethod = "fallbackRateLimiterApplyFilter")
+    @Bulkhead(name = "storage-service", fallbackMethod = "fallbackBulkheadApplyFilter", type = Bulkhead.Type.THREADPOOL)
+    public ResourceResponseDTO compressImage(CompressImageDTO compressImageDTO) throws IOException {
+
+        var imageRes = storageClient.findImageById(compressImageDTO.id()).getBody();
+
+        if (Objects.isNull(imageRes)) {
+            throw new IllegalArgumentException("Erro ao encontrar imagem requisitada.");
         }
 
-        return createTempFile(outputStream.toByteArray());
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        Thumbnails
+                .of(imageRes.getInputStream())
+                .outputQuality(compressImageDTO.compressionQuality())
+                .scale(1.0)
+                .toOutputStream(outputStream);
+
+        return new ResourceResponseDTO(imageRes.getFilename(), createTempFile(outputStream.toByteArray()));
     }
 
     private Resource createTempFile(byte[] byteArray) {
         return new ByteArrayResource(byteArray);
+    }
+
+    private ResourceResponseDTO fallbackCircuitBreakerApplyFilter(CompressImageDTO dto, Throwable throwable) {
+        logger.error("CircuitBreaker Fallback - Falha ao processar a requisição: id: {}, quality: {}", dto.id(), dto.compressionQuality());
+        logger.error("Fallback acionado devido a uma exceção: {}", throwable.getMessage());
+        return null;
+    }
+
+    private ResourceResponseDTO fallbackBulkheadApplyFilter(CompressImageDTO dto, Throwable throwable) {
+        logger.error("Bulkhead Fallback - Falha ao processar a requisição: id: {}, quality: {}", dto.id(), dto.compressionQuality());
+        logger.error("Fallback acionado devido a uma exceção: {}", throwable.getMessage());
+        return null;
+    }
+
+    private ResourceResponseDTO fallbackRetryApplyFilter(CompressImageDTO dto, Throwable throwable) {
+        logger.error("Retry Fallback - Falha ao processar a requisição: id: {}, quality: {}", dto.id(), dto.compressionQuality());
+        logger.error("Fallback acionado devido a uma exceção: {}", throwable.getMessage());
+        return null;
+    }
+
+    private ResourceResponseDTO fallbackRateLimiterApplyFilter(CompressImageDTO dto, Throwable throwable) {
+        logger.error("RateLimiter Fallback - Falha ao processar a requisição: id: {}, quality: {}", dto.id(), dto.compressionQuality());
+        logger.error("Fallback acionado devido a uma exceção: {}", throwable.getMessage());
+        return null;
     }
 }
 
--- format-service/src/main/java/com/pixelwave/formatservice/FormatServiceApplication.java
+++ format-service/src/main/java/com/pixelwave/formatservice/FormatServiceApplication.java
@@ -3,13 +3,15 @@ package com.pixelwave.formatservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class FormatServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FormatServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FormatServiceApplication.class, args);
+    }
 
 }
