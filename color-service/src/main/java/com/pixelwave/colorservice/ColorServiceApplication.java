4b46fc6a217c598c59f91b90f3a4cbdb12624a70
Felipe Lima <felipe.lima.092@ufrn.edu.br>
2023-12-21 22:29:59 -0300

    refactor: remoção de dependências desnecessárias

--- color-service/src/main/java/com/pixelwave/colorservice/ColorServiceApplication.java
+++ color-service/src/main/java/com/pixelwave/colorservice/ColorServiceApplication.java
@@ -3,11 +3,9 @@ package com.pixelwave.colorservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
 public class ColorServiceApplication {
 
     public static void main(String[] args) {
